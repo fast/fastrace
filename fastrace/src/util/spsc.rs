d2391de878065ed43abaa303f304fd7adf29d397
tison <wander4096@gmail.com>
2025-12-17 22:27:16 +0800

    chore: code tidy over spsc
    Signed-off-by: tison <wander4096@gmail.com>

--- fastrace/src/util/spsc.rs
+++ fastrace/src/util/spsc.rs
@@ -1,4 +1,20 @@
+// Copyright 2024 FastLabs Developers
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This file is derived from [1] under the original license header:
 // Copyright 2020 TiKV Project Authors. Licensed under Apache-2.0.
+// [1]: https://github.com/tikv/minitrace-rust/blob/v0.6.4/minitrace/src/util/spsc.rs
 
 use std::collections::VecDeque;
 
@@ -9,18 +25,13 @@ use rtrb::RingBuffer;
 
 pub fn bounded<T>(capacity: usize) -> (Sender<T>, Receiver<T>) {
     let (tx, rx) = RingBuffer::new(capacity);
-    (
-        Sender {
-            tx,
-            pending_messages: VecDeque::new(),
-        },
-        Receiver { rx },
-    )
+    let pending_msgs = VecDeque::new();
+    (Sender { tx, pending_msgs }, Receiver { rx })
 }
 
 pub struct Sender<T> {
     tx: Producer<T>,
-    pending_messages: VecDeque<T>,
+    pending_msgs: VecDeque<T>,
 }
 
 pub struct Receiver<T> {
@@ -32,39 +43,42 @@ pub struct ChannelClosed;
 
 impl<T> Sender<T> {
     #[inline]
-    pub fn is_under_pressure(&self) -> bool {
+    pub(crate) fn is_under_pressure(&self) -> bool {
         let capacity = self.tx.buffer().capacity();
         self.tx.slots() * 2 < capacity
     }
 
     pub fn send(&mut self, value: T) {
-        while let Some(value) = self.pending_messages.pop_front() {
-            if let Err(PushError::Full(value)) = self.tx.push(value) {
-                self.pending_messages.push_front(value);
-                break;
+        while let Some(pending_value) = self.pending_msgs.pop_front() {
+            if let Err(PushError::Full(pending_value)) = self.tx.push(pending_value) {
+                self.pending_msgs.push_front(pending_value);
+                self.pending_msgs.push_back(value);
+                return;
             }
         }
 
         if let Err(PushError::Full(value)) = self.tx.push(value) {
-            self.pending_messages.push_back(value);
+            self.pending_msgs.push_back(value);
         }
     }
 }
 
 impl<T> Drop for Sender<T> {
     fn drop(&mut self) {
-        for command in self.pending_messages.drain(..) {
-            drop(self.tx.push(command));
+        for msg in std::mem::take(&mut self.pending_msgs) {
+            self.tx.push(msg).ok();
         }
     }
 }
 
 impl<T> Receiver<T> {
     pub fn try_recv(&mut self) -> Result<Option<T>, ChannelClosed> {
-        match self.rx.pop() {
-            Ok(val) => Ok(Some(val)),
-            Err(_) if self.rx.is_abandoned() => Err(ChannelClosed),
-            Err(_) => Ok(None),
+        if let Ok(val) = self.rx.pop() {
+            Ok(Some(val))
+        } else if self.rx.is_abandoned() {
+            Err(ChannelClosed)
+        } else {
+            Ok(None)
         }
     }
 }
